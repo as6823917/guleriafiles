3311568c87a8e94310be56d865c1b5012d2c93bb
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-07-31 20:21:48 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -37,5 +37,5 @@ https://t.me/guleriamovies)
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Tutorial : [Click Me](https://t.me/guleriamovies)
 """
