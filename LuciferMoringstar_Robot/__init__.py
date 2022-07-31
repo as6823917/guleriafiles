053cfdedc823e666f22710186ac0913e91866c72
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-07-31 20:18:25 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,8 +30,8 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [GULERIA ](
+https://t.me/guleriamovies)
 
 ➥ Language : Python3
 
